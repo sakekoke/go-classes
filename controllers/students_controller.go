ef3fee5ed5cf4ec98b3dba53aec9a03c0c71efff
sakekoke <sakekoke42@gmail.com>
2021-11-11 19:50:18 +0600

    student CRUD done

--- controllers/students_controller.go
+++ controllers/students_controller.go
@@ -27,6 +27,38 @@ func (controller *Controller) CreateStudent(c echo.Context) error {
 	return c.String(http.StatusOK, "student created!")
 }
 
+func (controller *Controller) UpdateStudent(c echo.Context) error {
+	id := c.Param("id")
+	firstName := c.FormValue("first_name")
+	lastName := c.FormValue("last_name")
+	gender := c.FormValue("gender")
+
+	query := `UPDATE students
+	SET "first_name" = $1, "last_name" = $2, "gender" = $3 
+	WHERE "id" = $4;`
+	_, err := controller.DB.Exec(context.Background(), query, firstName, lastName, gender, id)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	return c.String(http.StatusOK, "student updated!")
+}
+
+func (controller *Controller) DeleteStudent(c echo.Context) error {
+	id := c.Param("id")
+
+	query := `DELETE FROM students
+	WHERE "id" = $1;`
+	_, err := controller.DB.Exec(context.Background(), query, id)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	return c.String(http.StatusOK, "student deleted!")
+}
+
 func (controller *Controller) GetAllStudents(c echo.Context) error {
 	query := `SELECT * FROM students;`
 	rows, err := controller.DB.Query(context.Background(), query)
@@ -43,7 +75,7 @@ func (controller *Controller) GetAllStudents(c echo.Context) error {
 			log.Fatal(err)
 		}
 		students = append(students, models.Student{
-			ID:        values[1].(int32),
+			ID:        values[0].(int32),
 			FirstName: values[1].(string),
 			LastName:  values[2].(string),
 			Gender:    values[3].(int32),
